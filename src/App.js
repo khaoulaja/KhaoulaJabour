ffdfbf3e2c996103a5843ee4b85617951be79e56
khaoula <khaoulajabour4@gmail.com>
2022-03-06 03:40:32 -0500

    create header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import Header from '../src/components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
+    <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () =>{
+
+    return (
+        <header>
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/">
+                  <h1 className="logo">Khaoula Jabour</h1>
+              </a>
+              
+            </div>
+          </nav>
+    </header>
+    )
+}
+export default Header;
